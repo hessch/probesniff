11a1fb7981f1ca1523b852ea60ca8652f8fa6037
Hessel Schut <hessel@isquared.nl>
2016-01-21 17:02:54 +0100

    disable promiscuous mode during frame processing

--- probesniff.ino
+++ probesniff.ino
@@ -4,6 +4,7 @@ extern "C" {
 }
 #include "network_80211.h"
 
+#define CHAN 6
 // expand int[6] to argument list (for snprintf)
 #define args6(a) a[0], a[1], a[2], a[3], a[4], a[5]
 
@@ -11,7 +12,8 @@ void handle_pkt(uint8_t *buf, uint16 len) {
   char ssid_buf[32];
   lpframectrl_80211 framectrl;
   if (len < 64) return;
-
+  
+  wifi_promiscuous_enable(0);
   buf += sizeof(struct RxControl);
   struct probe_request_80211 *probe_buf = (struct probe_request_80211*) buf;
 
@@ -31,13 +33,15 @@ void handle_pkt(uint8_t *buf, uint16 len) {
         os_memset(ssid_buf, 0, 32);
         os_memcpy(ssid_buf, (uint8_t *)tag + 2, tag->tag_length);
 
-        char logbuf[80];
-        snprintf(logbuf, sizeof(logbuf), "Probe request from %s for %s (%d octets remain)", 
-          srcaddr, ssid_buf, sizeof(buf) - sizeof(tag));
+        char logbuf[81];
+        /* snprintf(logbuf, sizeof(logbuf), "Probe request from %s for %s (%d octets remain)", 
+          srcaddr, ssid_buf, sizeof(buf) - sizeof(tag)); */
+        snprintf(logbuf, sizeof(logbuf), "Probe request from %s for %s", srcaddr, ssid_buf);
         Serial.println(logbuf);
       }
     }
   }
+  wifi_promiscuous_enable(1);
 }
 
 
@@ -47,6 +51,7 @@ void setup() {
   wifi_set_opmode(WIFI_STA);
   wifi_promiscuous_enable(1);
   wifi_set_promiscuous_rx_cb(handle_pkt);
+  wifi_set_channel(CHAN);
 }
 
 void loop() {  }
